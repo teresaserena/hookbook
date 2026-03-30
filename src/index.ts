00b69ae914c6b3d79330d60a45d59463c2b517e0
= <=>
2026-03-29 23:03:02 -0600

    adding a simple express frontend and fleshing out some other scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.listen(PORT, () => {
+  console.log(`Hookbook running at http://localhost:${PORT}`);
+});
